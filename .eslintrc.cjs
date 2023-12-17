1794c1256c9ddf318866eebfbb7ff34514b6cbc6
Oliver, Ryan <39997012+oliver-ryan9008@users.noreply.github.com>
2023-12-16 23:41:57 -0600

    fix rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,8 +21,7 @@ module.exports = {
     '@typescript-eslint/indent': 'off',
     'react/prop-types': 'off',
     "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off"
-    indent: 'off',
+    "react/react-in-jsx-scope": "off",
     'multiline-ternary': 'off'
   },
   globals: {
