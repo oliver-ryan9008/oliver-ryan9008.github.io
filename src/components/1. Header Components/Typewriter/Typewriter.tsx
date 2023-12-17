124e55777869dcaa9d1dd1c8a11ea643fd3226e5
Oliver, Ryan <39997012+oliver-ryan9008@users.noreply.github.com>
2023-12-17 15:28:04 -0600

    Update Typewriter.tsx

--- src/components/1. Header Components/Typewriter/Typewriter.tsx
+++ src/components/1. Header Components/Typewriter/Typewriter.tsx
@@ -1,51 +1,59 @@
-import React, { useState, useEffect, useMemo } from 'react';
-import { typeWriterText } from '../../../your_info'
-import './Typewriter.css';
+/* eslint-disable @typescript-eslint/naming-convention */
+import React, { useState, useEffect, useMemo } from "react"
+import { typeWriterText } from "../../../your_info"
+import "./Typewriter.css"
 
-
-const typing_speed = 150;
-const backspace_speed = 50;
+const typing_speed = 200
+const backspace_speed = 90
 
 function Typewriter() {
-  const [skill, setSkill] = useState("");
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [index, setIndex] = useState(0);
-  const [isFullyDisplayed, setIsFullyDisplayed] = useState(false);
+  const [skill, setSkill] = useState("")
+  const [isDeleting, setIsDeleting] = useState(false)
+  const [index, setIndex] = useState(0)
+  const [isFullyDisplayed, setIsFullyDisplayed] = useState(false)
 
   useEffect(() => {
-    let interval = setInterval(() => {
-      if (isDeleting) {
-        setSkill(prevSkill => prevSkill.substring(0, prevSkill.length - 1));
-        if (skill === '') {
-          setIsDeleting(false);
-          setIsFullyDisplayed(false);
-        }
-      } else {
-        const nextSkill = typeWriterText[index];
-        setSkill(prevSkill => nextSkill.substring(0, prevSkill.length + 1));
-        if (skill === nextSkill) {
-          setIsFullyDisplayed(true);
-          setTimeout(() => {
-            setIsDeleting(true);
+    const interval = setInterval(
+      () => {
+        if (isDeleting) {
+          setSkill((prevSkill) => prevSkill.substring(0, prevSkill.length - 1))
+          if (skill === "") {
+            setIsDeleting(false)
+            setIsFullyDisplayed(false)
+          }
+        } else {
+          const nextSkill = typeWriterText[index]
+          setSkill((prevSkill) => nextSkill.substring(0, prevSkill.length + 1))
+          if (skill === nextSkill) {
+            setIsFullyDisplayed(true)
             setTimeout(() => {
-              setIsFullyDisplayed(false);
-              setIndex((index + 1) % typeWriterText.length);
-            }, 500);
-          }, 1000);
+              setIsDeleting(true)
+              setTimeout(() => {
+                setIsFullyDisplayed(false)
+                setIndex((index + 1) % typeWriterText.length)
+              }, 500)
+            }, 1000)
+          }
         }
-      }
-    }, isFullyDisplayed ? backspace_speed : typing_speed);
+      },
+      isFullyDisplayed ? backspace_speed : typing_speed
+    )
 
-    return () => clearInterval(interval);
-  }, [skill, isDeleting, index, isFullyDisplayed]);
+    return () => {
+      clearInterval(interval)
+    }
+  }, [skill, isDeleting, index, isFullyDisplayed])
 
   const blinkingCursor = useMemo(() => {
-    return isFullyDisplayed ? '' : <span className="blinking-cursor">_</span>;
-  }, [isFullyDisplayed]);
+    return isFullyDisplayed ? "" : <span className="blinking-cursor">_</span>
+  }, [isFullyDisplayed])
 
   return (
-    <p>{skill}{blinkingCursor}</p>
-  );
+    <p>
+      {skill}
+      {blinkingCursor}
+    </p>
+  )
 }
 
-export default Typewriter;
+export default Typewriter
